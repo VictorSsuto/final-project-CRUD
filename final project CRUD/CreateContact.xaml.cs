f560434ce41c9fe3c67478f8659fbf00c5adb461
VictorSsuto <83074897+VictorSsuto@users.noreply.github.com>
2021-12-14 12:53:53 -0500

    Add project files.

---
+++ final project CRUD/CreateContact.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace final_project_CRUD
+{
+    /// <summary>
+    /// Interaction logic for CreateContact.xaml
+    /// </summary>
+    public partial class CreateContact : Window
+    {
+        public CreateContact()
+        {
+            InitializeComponent();
+        }
+    }
+}
