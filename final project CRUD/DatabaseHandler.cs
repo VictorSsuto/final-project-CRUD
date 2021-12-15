1b290d741843fc07689885a9215d5e17099e2847
Matthew Tan <1931243@champlaincollege.qc.ca>
2021-12-15 15:33:18 -0500

    added tge imported CSV in the database handler class

---
+++ final project CRUD/DatabaseHandler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace final_project_CRUD
+{
+    class DatabaseHandler
+    {
+        public void AddImportedCSV()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand($"INSERT INTO Contact([First Name], [Last Name], Age, Email, [Phone Number]) VALUES(@firstName, @lastName, @age, @email, @phoneNumber)", connection);
+
+                try
+                {
+                    connection.Open();
+
+                    command.Parameters.AddWithValue("@firstName", DbType.String);
+                    command.Parameters.AddWithValue("@lastName", DbType.String);
+                    command.Parameters.AddWithValue("@age", DbType.String);
+                    command.Parameters.AddWithValue("@email", DbType.String);
+                    command.Parameters.AddWithValue("@phoneNumber", DbType.String);
+
+                    foreach (var contact in ContactCollection.importedCSVContacts)
+                    {
+                        if (contact.Email == null)
+                        {
+                            command.Parameters[0].Value = contact.FirstName;
+                            command.Parameters[1].Value = contact.LastName;
+                            command.Parameters[2].Value = contact.Age;
+                            command.Parameters[3].Value = DBNull.Value;
+                            command.Parameters[4].Value = contact.PhoneNumber;
+
+                            command.ExecuteNonQuery();
+                            continue;
+                        }
+                        if (contact.PhoneNumber == null)
+                        {
+                            command.Parameters[0].Value = contact.FirstName;
+                            command.Parameters[1].Value = contact.LastName;
+                            command.Parameters[2].Value = contact.Age;
+                            command.Parameters[3].Value = contact.Email;
+                            command.Parameters[4].Value = DBNull.Value;
+
+                            command.ExecuteNonQuery();
+                            continue;
+                        }
+                        else
+                        {
+                            command.Parameters[0].Value = contact.FirstName;
+                            command.Parameters[1].Value = contact.LastName;
+                            command.Parameters[2].Value = contact.Age;
+                            command.Parameters[3].Value = contact.Email;
+                            command.Parameters[4].Value = contact.PhoneNumber;
+
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+                catch (SqlException e)
+                {
+                    MessageBox.Show("Something went wrong" + e.ToString(), "Oops!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
+        
